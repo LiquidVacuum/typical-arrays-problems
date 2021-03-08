9ad76ff0149f147871016072e6c6b2ef3532ae78
LiquidVacuum <venomandroid@gmail.com>
2021-03-08 14:50:45 +0200

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let min = Infinity;
+  for (const item of array) {
+    min = item < min ? item : min ;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let max = -Infinity;
+  for (const item of array) {
+    max = item > max ? item : max ;
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+    if (array == undefined || array.length == 0) return 0;
+    let avg = 0;
+    for (const item of array) {
+        avg += item;
+    }
+    avg = avg / array.length;
+    return avg;
 }
